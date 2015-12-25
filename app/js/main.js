4b68748aa31d1ae1784ad2e8b96e84bc3fd5b050
luyaotsung <luyaotsung@gmail.com>
2015-12-25 20:28:56 +0800

    uplaod code

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,8 @@
     require('angular-route');
     require('angular-animate');
     require('angular-base64-upload');
+    require('angular-utf8-base64');
+    require('angular-file-saver');
 
     var uibs = require('angular-ui-bootstrap');
 
@@ -13,7 +15,8 @@
     var ubCtrl = require('./controllers/ubctrl');
     var uploadCtrl = require('./controllers/uploadctrl');
 
-    angular.module('SampleApp', [uibs,'ngRoute', 'ngAnimate','naif.base64'])
+     // angular.module('SampleApp',[uibs,'utf8-base64','ngFileSaver']).controller('MenuCtrl', ['FileSaver','Blob','base64','$scope','$window',menuCtrl]);
+    angular.module('SampleApp', [uibs,'utf8-base64','ngFileSaver','ngRoute', 'ngAnimate','naif.base64'])
     .config([
         '$routeProvider',
         '$httpProvider',
@@ -42,9 +45,9 @@
     //Load controller
     .controller('MainController', ['$scope','$http', mainCtrl])
     .controller('UBController', ['$scope', ubCtrl])
-    .controller('UploadController', ['$scope', '$http',uploadCtrl]);
+    .controller('UploadController', ['FileSaver','Blob','base64','$scope', '$http',uploadCtrl]);
 
-    var menuCtrl = require('./controllers/menuctrl');
-    angular.module('SampleApp').controller('MenuCtrl', ['$scope','$window',menuCtrl]);
+     var menuCtrl = require('./controllers/menuctrl');
+     angular.module('SampleApp').controller('MenuCtrl', ['$scope','$window',menuCtrl]);
 
 }());
